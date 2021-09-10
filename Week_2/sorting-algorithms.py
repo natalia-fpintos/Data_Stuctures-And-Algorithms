5a25f35ad0fc80f0d3b1ec7f0c7d5a14be6a40a2
Natalia Fernández <natalia.fpintos@gmail.com>
2021-09-10 11:49:12 +0100

    Fix issue with selection sort & extract swap method

--- Week_2/sorting-algorithms.py
+++ Week_2/sorting-algorithms.py
@@ -1,14 +1,20 @@
 values = [2, 9, 7, 5, 8, 3, 1, 6, 4]
 
+
 def selection_sort(values):
-  for i in range(len(values)):
-    smallest = i
-    for j in range(i, len(values)):
-      if values[j] < values[smallest]:
-        smallest = values[j]
-    print("swapping {} for {}".format(values[i], values[smallest]))
-    values[i], values[smallest] = values[smallest], values[i]
-    
+    for i in range(len(values)):
+        smallest = i
+        for j in range(i, len(values)):
+            if values[j] < values[smallest]:
+                smallest = j
+        print("swapping {} for {}".format(values[i], values[smallest]))
+        swap(i, smallest, values)
+
+
+def swap(idx1, idx2, list_ref):
+    list_ref[idx1], list_ref[idx2] = list_ref[idx2], list_ref[idx1]
+
+
 if __name__ == "__main__":
     selection_sort(values)
     print(values)
