1c59f547b53ef1bf8d5c7cb2c909aff1072403cb
Natalia Fernández <natalia.fpintos@gmail.com>
2021-10-01 10:11:09 +0100

    Fix file formatting

--- week_5/stack.py
+++ week_5/stack.py
@@ -1,79 +1,76 @@
 class Stack:
-  def __init__(self, max_size):
-    self.max_size = max_size
-    self.stack_data = [None for _ in range(max_size)]
-    self.top = -1
+    def __init__(self, max_size):
+        self.max_size = max_size
+        self.stack_data = [None for _ in range(max_size)]
+        self.top = -1
 
+    def is_empty(self):
+        return self.top == -1
 
-  def is_empty(self):
-    return self.top == -1
+    def is_full(self):
+        return self.top == self.max_size - 1
 
+    def peek(self):
+        return self.stack_data[self.top]
 
-  def is_full(self):
-    return self.top == self.max_size - 1
+    def pop(self):
+        item = self.stack_data[self.top]
+        self.stack_data[self.top] = None
+        self.top -= 1
+        return item
 
-  def peek(self):
-    return self.stack_data[self.top]
-
-  def pop(self):
-    item = self.stack_data[self.top]
-    self.stack_data[self.top] = None
-    self.top -= 1
-    return item
-
-  def push(self, value):
-    self.top += 1
-    self.stack_data[self.top] = value
+    def push(self, value):
+        self.top += 1
+        self.stack_data[self.top] = value
 
 
 class Link:
-  def __init__(self, data):
-    self.data = data
-    self.next = None
+    def __init__(self, data):
+        self.data = data
+        self.next = None
 
 
 class LinkedStack:
-  def __init__(self):
-    self.first = None
+    def __init__(self):
+        self.first = None
 
-  def insert(self, new_link):
-    new_link.next = self.first
-    self.first = new_link
+    def insert(self, new_link):
+        new_link.next = self.first
+        self.first = new_link
 
-  def remove(self):
-    item = self.first
-    self.first = self.first.next
-    return item
+    def remove(self):
+        item = self.first
+        self.first = self.first.next
+        return item
 
-  def display(self):
-    current = self.first
-    while current is not None:
-      print(current.data)
-      current = current.next
+    def display(self):
+        current = self.first
+        while current is not None:
+            print(current.data)
+            current = current.next
 
 
 if __name__ == "__main__":
-  print("Stack exercises:")
+    print("Stack exercises:")
 
-  a_stack = Stack(5)
+    a_stack = Stack(5)
 
-  for i in range(4):
-    a_stack.push(i)
-    
-  print(a_stack.stack_data)
+    for i in range(4):
+        a_stack.push(i)
 
-  while not a_stack.is_empty():
-    print(f"Stack item popped: {a_stack.pop()}")
+    print(a_stack.stack_data)
 
-  print("Linked list stack exercises:")
+    while not a_stack.is_empty():
+        print(f"Stack item popped: {a_stack.pop()}")
 
-  a_linked_stack = LinkedStack()
+    print("Linked list stack exercises:")
 
-  for i in range(10):
-    a_linked_stack.insert(Link(i))
+    a_linked_stack = LinkedStack()
 
-  a_linked_stack.display()
+    for i in range(10):
+        a_linked_stack.insert(Link(i))
 
-  while a_linked_stack.first is not None:
-    print(f"Stack link popped: {a_linked_stack.remove().data}")
+    a_linked_stack.display()
 
+    while a_linked_stack.first is not None:
+        print(f"Stack link popped: {a_linked_stack.remove().data}")
