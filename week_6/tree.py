4a7c97c6f935e5bbf90699d668108245aa300ed1
Natalia Fernández Pintos <natalia.fpintos@gmail.com>
2021-10-01 11:35:00 +0000

    Add node for tree structure

---
+++ week_6/tree.py
@@ -0,0 +1,19 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.left_child = None
+    self.right_child = None
+    
+  def display(self):
+    print(self.data)
+    
+  
+class Tree:
+  def __init__(self):
+    self.root = None
+  
+  def find(self, item):
+    pass
+    
+  def display_in_order(self):
+    pass
