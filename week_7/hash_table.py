ec420efd1515920df20b6d2456682bc508a14a6f
Natalia Fernández <natalia.fpintos@gmail.com>
2021-10-07 10:51:13 +0100

    Finish initial hash table implementation

--- week_7/hash_table.py
+++ week_7/hash_table.py
@@ -15,11 +15,23 @@ class HashTable:
                 print(f"Index: {k}. Key: {v.key}. Name: {v.name}.")
 
     def hash_function(self, key):
-        pass
+        return key % self.size
 
     def insert(self, item):
-        pass
+        key = item.key
+        h = self.hash_function(key)
+
+        while self.data[h] is not None:
+            h += 1
+            h %= self.size
+
+        self.data[h] = item
 
 
 if __name__ == "__main__":
-    pass
+    table = HashTable(17)
+    table.insert(GameObject(10, "Bridge"))
+    table.insert(GameObject(5, "Player"))
+    table.insert(GameObject(30, "Tank"))
+    table.insert(GameObject(16, "AI"))
+    table.display()
